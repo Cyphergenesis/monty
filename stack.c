b5704a23cc499d44faaad712bb5539436f186aed
Cyphergenesis <josquilani200>
2023-06-23 00:48:03 +0100

    sets format of the data

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * _stack - prints the top
+ * @head: stack head
+ * @count: line_number
+ * Return: no return
+*/
+void _stack(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	bus.lifi = 0;
+}
