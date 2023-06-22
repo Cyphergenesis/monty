983c5b51dfc7450ff00067ad5166047c5195c839
Olabanji Enioluwayan <olabanjienioluwayan@gmail.com>
2023-06-22 16:07:55 -0700

    implement the div opcode

---
+++ div.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+/**
+ * _div - divides the top two elements of the stack.
+ * @head: stack head
+ * @count: line_number
+ * Return: no return
+*/
+void _div(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+	int length = 0, au_x;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	au_x = h->next->n / h->n;
+	h->next->n = au_x;
+	*head = h->next;
+	free(h);
+}
